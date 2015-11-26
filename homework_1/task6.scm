b5eea17ec2ffcab914ded60c0591805ff82920dc
zlatkov <alexander.z.zlatkov@gmail.com>
2015-11-26 04:34:34 +0200

    Solved task 6

---
+++ homework_1/task6.scm
@@ -0,0 +1,17 @@
+#lang racket
+
+(define (merge-sorted lst1 lst2 pred) 
+  (cond
+    ((null? lst1) lst2)
+    ((null? lst2) lst1)
+    ((pred (car lst1) (car lst2)) (cons (car lst1) (merge-sorted (cdr lst1) lst2 pred)))
+    (else (cons (car lst2) (merge-sorted lst1 (cdr lst2) pred)))
+    )
+  )
+
+(define (merge-sort lst pred)
+  (if (<= (length lst) 1)
+      lst
+      (merge-sorted (merge-sort (take lst (quotient (length lst) 2)) pred) (merge-sort (drop lst (quotient (length lst) 2)) pred) pred)
+      )
+  )
